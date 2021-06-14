e1f053ab6a0f9268e676a132f88420d48d373ee8
clefcodex <iwewale1@gmail.com>
2021-06-14 02:50:50 +0100

    create backend branch

---
+++ pjt-101/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\ayaloonline-be-pjt-101\pjt-101\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

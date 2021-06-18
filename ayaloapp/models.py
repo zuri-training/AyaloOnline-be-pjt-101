2b2ed307f00104cb78a22cffb5426f609572371d
* <amaka013@gmail.com>
2021-06-18 22:10:47 +0100

    Part of the code for the signup via email

--- ayaloapp/models.py
+++ ayaloapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from authemail.models import EmailUserManager
+from authemail.models import EmailAbstractUser
 # Create your models here.
+ 
+class MyUser(EmailAbstractUser):
+   objects=EmailUserManager
--- ayaloconfig/settings.py
+++ ayaloconfig/settings.py
@@ -37,7 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    "authemail",
+    "ayaloapp"
+
+
 ]
+REST_FRAMEWORK = {
+	'DEFAULT_AUTHENTICATION_CLASSES': (
+		'rest_framework.authentication.TokenAuthentication',
+	)
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -123,3 +135,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'ayaloapp.MyUser'

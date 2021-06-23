b75bbc8f50bdd8e0958abe573132875ed279aed5
Maureen Ononiwu <59079323+Chinwendu20@users.noreply.github.com>
2021-06-23 07:42:43 +0100

    Updated settings.py with whitenoise middleware

--- ayaloconfig/settings.py
+++ ayaloconfig/settings.py
@@ -78,6 +78,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'ayaloconfig.urls'

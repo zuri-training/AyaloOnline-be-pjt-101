f493af39c15e56a236a60c1e50213e830e69cee6
Chinwendu2o <amaka013@gmail.com>
2021-07-15 21:23:50 +0100

    Update

--- ayaloconfig/settings.py
+++ ayaloconfig/settings.py
@@ -185,11 +185,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'ayaloapp.MyUser'
 
-EMAIL_FROM = config('NAME')
+EMAIL_FROM = "Ayalo-Online"
 # EMAIL_BCC=config('EMAIL_ADDRESS')
 
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST=s'smtp.gmail.com'
+EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = config('HOST_USER')
 EMAIL_HOST_PASSWORD = config('PASSWORD')
